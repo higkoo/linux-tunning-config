820431f86e27920e8c5274e13f37c19f019baa00
higkoo <higkoohk@gmail.com>
2015-11-12 17:18:04 +0800

    动态计算 nf_conntrack hashsize
    动态计算 nf_conntrack hashsize 的大小 。

--- script/iptables.sh
+++ script/iptables.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
-# iptables-stop.sh status | stop | disable | tunning
-ipt_mod_conf="/etc/modprobe.d/iptables-blacklist.conf"
+# iptables.sh status | clean | disable | enable
+ipt_mod_conf="/etc/modprobe.d/iptables.conf"
+ipt_mod_list="ip_vs iptable_nat nf_nat_ipv4 ipt_MASQUERADE nf_nat nf_conntrack_ipv4 nf_defrag_ipv4 xt_conntrack nf_conntrack iptable_filter ip_tables xt_tcpudp xt_multiport xt_length xt_addrtype x_tables"
 nf_max=$(sysctl -e -n net.nf_conntrack_max)
 nf_cur=$(sysctl -e -n net.netfilter.nf_conntrack_count)
+ipt_hsize=$(grep 'MemTotal' /proc/meminfo | awk '{printf("%d",$2/16)}')
 
 fuck_ipt_mod(){
-/bin/cat > ${ipt_mod_conf} << _rm-ipt-modprobe
-blacklist iptable_nat
-blacklist ipt_REDIRECT
-blacklist nf_conntrack
-blacklist nf_conntrack_ftp
-blacklist nf_conntrack_ipv4
-blacklist nf_conntrack_ipv6
-blacklist nf_nat
-blacklist xt_conntrack
-blacklist xt_state
-_rm-ipt-modprobe
-for ipt_mod in $(awk '{print $NF}' ${ipt_mod_conf}); do
+echo '# disable iptables conntrack modules' > ${ipt_mod_conf}
+for ipt_mod in ${ipt_mod_list}; do
+	echo "blacklist ${ipt_mod}" >> ${ipt_mod_conf}
     modprobe -r ${ipt_mod}
 done
 }
 
 clean_ipt_rule(){
 iptables -F
-iptables -X
 iptables -Z
+iptables -X
 for ipt_table in $(cat /proc/net/ip_tables_names 2>/dev/null); do
     iptables -t ${ipt_table} -F
-    iptables -t ${ipt_table} -X
     iptables -t ${ipt_table} -Z
+    iptables -t ${ipt_table} -X
 done
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 }
 
-ipt_tunning(){
-/bin/cat > ${ipt_mod_conf} << _rm-ipt-modprobe
-options nf_conntrack hashsize=262144
-_rm-ipt-modprobe
-modprobe -r nf_conntrack && modprobe -a nf_conntrack
+ipt_enable(){
+echo "options nf_conntrack hashsize=${ipt_hsize}" > ${ipt_mod_conf} # /sys/module/nf_conntrack/parameters/hashsize
+for ipt_mod in ${ipt_mod_list}; do
+	modprobe -q -r ${ipt_mod} && modprobe -a ${ipt_mod}
+done
 dmesg --reltime | grep nf_conntrack | tail -2 2>/dev/null
 sysctl -e -w net.nf_conntrack_max=4194304
 sysctl -e -w net.ipv4.netfilter.ip_conntrack_max=4194304
@@ -58,20 +51,21 @@ status)
         echo "nf_conntrack used: ${nf_cur}/${nf_max}."
     fi
     ;;
-stop)
+clean)
     clean_ipt_rule
     $0 status
     ;;
 disable)
-    $0 stop
+    clean_ipt_rule
     fuck_ipt_mod
+    $0 status
     ;;
-tunning)
-    ipt_tunning
+enable)
+    ipt_enable
     $0 status
 ;;
 *)
-    echo "Usage: $0 {status|stop|disable|tunning}"
+    echo "Usage: $0 {status|clean|disable|enable}"
     exit 2
     ;;
 esac
