d4f579f87e0074b16087feca7cbe3f63045e0788
higkoo <higkoohk@gmail.com>
2015-12-17 11:44:50 +0800

    根据依赖关系，调整mod的卸载顺序
    增加stop功能

--- script/iptables-switch.sh
+++ script/iptables-switch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # iptables.sh status | disable | enable
 ipt_mod_conf="/etc/modprobe.d/iptables.conf"
-ipt_mod_list="ip_vs iptable_nat nf_nat_ipv4 ipt_MASQUERADE nf_nat nf_conntrack_ipv4 nf_defrag_ipv4 xt_conntrack nf_conntrack iptable_filter ip_tables xt_tcpudp xt_multiport xt_length xt_addrtype x_tables"
+ipt_mod_list="iptable_filter xt_length xt_tcpudp ipt_MASQUERADE xt_conntrack xt_multiport xt_addrtype ip_vs iptable_nat nf_conntrack_ipv4 nf_defrag_ipv4 nf_nat_ipv4 nf_nat nf_conntrack x_tables ip_tables"
+disable_ipt_mod=0
 nf_max=$(sysctl -e -n net.nf_conntrack_max)
 nf_cur=$(sysctl -e -n net.netfilter.nf_conntrack_count)
 ipt_hsize=$(grep 'MemTotal' /proc/meminfo | awk '{printf("%d",$2/64)}')
 
 fuck_ipt_mod(){
-echo '# disable iptables conntrack modules' > ${ipt_mod_conf}
+[ $disable_ipt_mod ] && echo '# disable iptables conntrack modules' > ${ipt_mod_conf}
 for ipt_mod in ${ipt_mod_list}; do
-    echo "blacklist ${ipt_mod}" >> ${ipt_mod_conf}
+    [ $disable_ipt_mod ] && echo "blacklist ${ipt_mod}" >> ${ipt_mod_conf}
     modprobe -r ${ipt_mod}
 done
 }
@@ -22,6 +23,7 @@ for ipt_table in $(cat /proc/net/ip_tables_names 2>/dev/null); do
     iptables -t ${ipt_table} -F
     iptables -t ${ipt_table} -Z
     iptables -t ${ipt_table} -X
+    echo "Table: ${ipt_table} stopped."
 done
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
@@ -31,7 +33,7 @@ iptables -P FORWARD ACCEPT
 ipt_enable(){
 echo "options nf_conntrack hashsize=${ipt_hsize}" > ${ipt_mod_conf} # /sys/module/nf_conntrack/parameters/hashsize
 for ipt_mod in ${ipt_mod_list}; do
-	modprobe -q -r ${ipt_mod} && modprobe -a ${ipt_mod}
+    modprobe -a ${ipt_mod}
 done
 dmesg --reltime | grep nf_conntrack | tail -2 2>/dev/null
 sysctl -e -w net.nf_conntrack_max=4194304
@@ -51,6 +53,10 @@ status)
         echo "nf_conntrack used: ${nf_cur}/${nf_max}."
     fi
     ;;
+stop)
+    disable_ipt_mod=1
+    $0 disable
+    ;;
 disable)
     clean_ipt_rule
     fuck_ipt_mod
@@ -61,7 +67,7 @@ enable)
     $0 status
 ;;
 *)
-    echo "Usage: $0 {status|disable|enable}"
+    echo "Usage: $0 {status|stop|disable|enable}"
     exit 2
     ;;
 esac
